42e651d850b679c2767a371c6a827d72ba2a22fd
cornelk <github@codejuggle.dj>
2017-02-13 16:19:04 +0700

    b.RunParallel removed

--- slice_test.go
+++ slice_test.go
@@ -6,50 +6,42 @@ import (
 
 func BenchmarkSliceReadRange(b *testing.B) {
 	m := generateIntSlice(b)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			for i, e := range m {
-				checkItem(b, i, e)
-			}
+	for n := 0; n < b.N; n++ {
+		for i, e := range m {
+			checkItem(b, i, e)
 		}
-	})
+	}
 }
 
 func BenchmarkSliceReadForward(b *testing.B) {
 	m := generateIntSlice(b)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			for i := 0; i < BenchMarkSize; i++ {
-				e := m[i]
-				checkItem(b, i, e)
-			}
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < BenchMarkSize; i++ {
+			e := m[i]
+			checkItem(b, i, e)
 		}
-	})
+	}
 }
 
 func BenchmarkSliceReadBackwards(b *testing.B) {
 	m := generateIntSlice(b)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			for i := BenchMarkSize - 1; i >= 0; i-- {
-				e := m[i]
-				checkItem(b, i, e)
-			}
+	for n := 0; n < b.N; n++ {
+		for i := BenchMarkSize - 1; i >= 0; i-- {
+			e := m[i]
+			checkItem(b, i, e)
 		}
-	})
+	}
 }
 
 func BenchmarkSliceReadLastItemFirst(b *testing.B) {
 	m := generateIntSlice(b)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			if m[BenchMarkSize-1] != BenchMarkSize-1 {
-				b.Fail()
-			}
-			for i := 0; i < BenchMarkSize; i++ {
-				e := m[i]
-				checkItem(b, i, e)
-			}
+	for n := 0; n < b.N; n++ {
+		if m[BenchMarkSize-1] != BenchMarkSize-1 {
+			b.Fail()
 		}
-	})
+		for i := 0; i < BenchMarkSize; i++ {
+			e := m[i]
+			checkItem(b, i, e)
+		}
+	}
 }
